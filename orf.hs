5c3550c38a40e202350b652d22f35745407b7b48
nirgle <=>
2018-04-06 12:31:53 -0400

    Refactor, use recursion instead of mapping on indices

--- orf.hs
+++ orf.hs
@@ -15,20 +15,6 @@ import NanoParsec          (Parser, string, oneOf, parse)
 type DNA = [Char]
 type RNA = [Char]
 
-toRNA :: DNA -> RNA
-toRNA = map f
-  where f 'T' = 'U'
-        f  x  =  x
-
-indices :: Eq a => [a] -> [a] -> [Int]
-indices []       _    = []
-indices haystack needle = go 0 haystack
-  where
-    go n []  = []
-    go n hay = if needle `isPrefixOf` hay
-               then n : go (n+1) (tail hay)
-               else     go (n+1) (tail hay)
-
 codon :: Parser String
 codon = do
   c1 <- oneOf "ACGU"
@@ -38,39 +24,46 @@ codon = do
   guard $ not $ isStop codon
   return codon
 
-stopCodon :: Parser String
-stopCodon = string "UAA" <|> string "UGA" <|> string "UAG"
+startCodon, stopCodon :: Parser String
+startCodon = string "AUG"
+stopCodon  = string "UAA" <|> string "UGA" <|> string "UAG"
 
 codonSequence :: Parser String
 codonSequence = do
-  codons <- many codon
-  _      <- stopCodon
-  return $ concat codons
+  start <- startCodon
+  seq   <- many codon
+  _     <- stopCodon
+  return $ start ++ concat seq
 
--- Find all candidate proteins from an RNA strand
-frames :: RNA -> [String]
-frames rna = 
-  let starts = indices rna "AUG"
-      mapped = map (\index -> let strand = drop index rna
-                              in  case parse codonSequence strand of
-                                    [(seq, _)] -> codonSeqToAminos seq
-                                    []         -> []
-                   ) starts
-  in  filter (not . null) mapped
+-- Find all candidate amino acid sequences in an RNA strand
+aminoSequences :: RNA -> [String]
+aminoSequences []  = []
+aminoSequences rna = let rest = aminoSequences $ tail rna
+                     in  case parse codonSequence rna of
+                           [(seq, _)] -> codonSeqToAminos seq : rest
+                           []         -> rest
 
-complement :: String -> String
-complement = map f
-  where  f 'A' = 'T'
-         f 'T' = 'A'
-         f 'C' = 'G'
-         f 'G' = 'C'
+proteins :: DNA -> [String]
+proteins dna = nub $ forward ++ backward
+  where
+    rna      = toRNA dna
+    forward  = aminoSequences rna
+    backward = aminoSequences (complement . reverse $ rna)
+
+    toRNA :: DNA -> RNA
+    toRNA = map f
+      where f 'T' = 'U'
+            f  x  =  x
+
+    complement :: RNA -> RNA
+    complement = map f
+      where  f 'A' = 'U'
+             f 'U' = 'A'
+             f 'C' = 'G'
+             f 'G' = 'C'
 
 main = do
   file <- readFile "orf.input"
   let dna = fastaSeq $ head $ parseFASTAdna file
-  let fwd = frames $ toRNA dna
-  let bwd = frames $ toRNA $ complement . reverse $ dna
-  let both = fwd ++ bwd
-  let distinct = nub both
-  print distinct
+  print $ proteins dna
 
