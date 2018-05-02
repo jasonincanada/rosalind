d5a187a3b26bd18a9b816955992845e5aaa5caa3
nirgle <=>
2018-05-02 15:29:10 -0400

    Correct overlap issue

--- pcov.hs
+++ pcov.hs
@@ -4,7 +4,14 @@
    Problem: Genome Assembly with Perfect Coverage
    URL:     http://rosalind.info/problems/pcov/
 
-   Remarks: This outputs almost the correct answer, but I had to do a text
+   Remarks: 2 May 2018
+
+            This revision gives the correct answer without needing to
+            manually remove the overlap.
+
+            1 May 2018
+
+            This outputs almost the correct answer, but I had to do a text
             search of the beginning of the strand to know where it started
             to overlap at the end so I could manually remove the overlap.
             So there is some room to improve this code so it gives the
@@ -33,18 +40,14 @@ coalgebra (dna, pool) = NodeF dna subs
 type Algebra f a = f a -> a
 
 algebra :: Algebra TreeF [(Int, DNA)]
-algebra (NodeF dna []     ) = [(1, dna)]
-algebra (NodeF dna strands) = [ (n+1, combine dna acc) | (n, acc) <- concat strands ]
-    where combine :: DNA -> DNA -> DNA
-          combine dna acc = if null acc
-                            then dna
-                            else head dna : acc
+algebra (NodeF dna []     ) = [ (  1, [last dna]) ]
+algebra (NodeF dna strands) = [ (n+1,  last dna : acc) | (n, acc) <- concat strands ]
 
 hylo :: Functor f => Coalgebra f b -> Algebra f a -> b -> a
 hylo coalg alg = alg . fmap (hylo coalg alg) . coalg
 
 main = do
-  file <- readFile "pcov.input"
+  file <- readFile "pcov.input.2"
   let fragments = lines file
   let count = length fragments
   let candidates = hylo coalgebra algebra (head fragments, tail fragments)
