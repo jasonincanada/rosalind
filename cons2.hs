21a15f01f02cdb412902da32346ae0cca2b647ab
jasonincanada <nirgle@gmail.com>
2018-05-31 10:49:23 -0400

    Treat Vec/Profile as monoids

--- cons2.hs
+++ cons2.hs
@@ -5,8 +5,8 @@
    URL:     http://rosalind.info/problems/cons/
 
    Remarks: This is a different, somewhat more Haskell-ish solution compared to cons.hs.
-            It uses addition of vectors to tally the profile counts.  cons.hs is
-            probably a better solution overall, but this one is a result of thinking
+            It treats vectors and profiles as monoids, with appropriate instances.  cons.hs
+            is probably a better solution overall, but this one is a result of thinking
             more in terms of transformations between structures instead of writing the
             "Haskell version" of the older imperative methods I would have used before.
 
@@ -16,6 +16,7 @@
 -}
 
 import Bioinformatics (fastaSeq, parseFASTAdna)
+import Data.Monoid    (Monoid, mempty, mappend, (<>))
 
 type Nucleotide = Char
 type DNA        = [Nucleotide]
@@ -29,26 +30,26 @@ instance Show Profile where
     where line nucleotide = [nucleotide] ++ ": " ++ unwords (tallies nucleotide)
           tallies n       = map (show . tally n) vecs
 
+instance Monoid Vec where
+  mempty = Vec 0 0 0 0
+  mappend (Vec a b c d) (Vec a' b' c' d') = Vec (a+a') (b+b') (c+c') (d+d')
+
+instance Monoid Profile where
+  mempty = Profile $ repeat (Vec 0 0 0 0)
+  mappend (Profile as) (Profile bs) = Profile $ zipWith (<>) as bs
+
 tally :: Nucleotide -> Vec -> Int
 tally 'A' (Vec a _ _ _) = a
 tally 'C' (Vec _ c _ _) = c
 tally 'G' (Vec _ _ g _) = g
 tally 'T' (Vec _ _ _ t) = t
 
-vec :: Nucleotide -> Vec
-vec 'A' = Vec 1 0 0 0
-vec 'C' = Vec 0 1 0 0
-vec 'G' = Vec 0 0 1 0
-vec 'T' = Vec 0 0 0 1
-
--- Add one strand to a profile
-process :: DNA -> Profile -> Profile
-process dna (Profile vecs) = Profile vecs'
-  where vecs'       = zipWith combine vecs dna
-        combine v d = v +++ vec d
-
-        (+++) :: Vec -> Vec -> Vec
-        (Vec a c g t) +++ (Vec a' c' g' t') = Vec (a+a') (c+c') (g+g') (t+t')
+toProfile :: DNA -> Profile
+toProfile dna = Profile $ map vec dna
+  where vec 'A' = Vec 1 0 0 0
+        vec 'C' = Vec 0 1 0 0
+        vec 'G' = Vec 0 0 1 0
+        vec 'T' = Vec 0 0 0 1
 
 -- All possible consensus strings for a profile
 consensuses :: Profile -> [DNA]
@@ -61,16 +62,12 @@ consensuses (Profile vecs) = go vecs
                               maxcount = maximum $ map snd tallies
                           in  map fst $ filter ((==maxcount) . snd) tallies
 
--- Start with an empty profile for strands of constant length n
-blank :: Int -> Profile
-blank n = Profile $ replicate n (Vec 0 0 0 0)
-
 main :: IO ()
 main = do
   file <- readFile "cons2.input"
   let parsed = parseFASTAdna file
-  let dna = map fastaSeq parsed
-  let len = length $ head dna
-  let profile = foldr process (blank len) dna
+  let dnas = map fastaSeq parsed
+  let profiles = map toProfile dnas
+  let profile = foldr (<>) mempty profiles
   putStrLn $ head $ consensuses profile
   print $ profile
